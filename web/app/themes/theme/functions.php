171b4e9e5cfe3afa92c2eb675a81e42b35691199
goullaWSL <gonzalo@upbrandsdesigns.com>
2022-03-08 17:04:19 -0300

    Fixes and ACF blocks template

--- web/app/themes/theme/functions.php
+++ web/app/themes/theme/functions.php
@@ -174,3 +174,27 @@ require get_template_directory() . '/inc/template-functions.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
+
+
+/**
+ * ACF Blocks
+ */
+
+add_action('acf/init', 'upbrands_blocks');
+function upbrands_blocks() {
+
+    // Check function exists.
+    if( function_exists('acf_register_block_type') ) {
+
+        // register a testimonial block.
+        acf_register_block_type(array(
+            'name'              => 'testimonial',
+            'title'             => __('Testimonial'),
+            'description'       => __('A custom testimonial block.'),
+            'render_template'   => 'template-parts/blocks/testimonial/testimonial.php',
+            'category'          => 'upbrands',
+            'icon'              => 'admin-comments',
+            'keywords'          => array( 'testimonial', 'quote' ),
+        ));
+    }
+}
